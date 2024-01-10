e7e16fc0ce02508ba7374ae75bb4bb1c72e553b8
rovesea <rovesea1@gmail.com>
2024-01-10 16:13:03 +0700

    exercise-2-haeder

--- react-2/exercise-2-harder/src/App.jsx
+++ react-2/exercise-2-harder/src/App.jsx
@@ -1,123 +1,96 @@
-import "./App.css";
+  import "./App.css";
 
-function App() {
+  function App() {
 
-  const posts = [
-    {
-      author: "User 1",
-      avatar: "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png",
-      time: "2 hours ago",
-      content: "This is my first post! 🎉",
-      image: "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
-    },
-    {
-      author: "User 2",
-      avatar: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png",
-      time: "4 hours ago",
-      content: "Enjoying a beautiful day outdoors! ☀️",
-      image: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
-    },
-    {
-      author: "User 3",
-      avatar: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png",
-      time: "1 day ago",
-      content: "Exploring new places and cultures. 🌍✈️",
-      image: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
-    }
-  ];
+    const posts = [
+      {
+        author: "User 1",
+        avatar: "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png",
+        time: "2 hours ago",
+        content: "This is my first post! 🎉",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
+      },
+      {
+        author: "User 2",
+        avatar: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png",
+        time: "4 hours ago",
+        content: "Enjoying a beautiful day outdoors! ☀️",
+        image: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
+      },
+      {
+        author: "User 3",
+        avatar: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png",
+        time: "1 day ago",
+        content: "Exploring new places and cultures. 🌍✈️",
+        image: "https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
+      }
+    ];
 
-//   return (
-//     <div id="app">
-//       <h1>Enter Data</h1>
-//       <PostContainer posts = {posts} />
-//       <FeedSection posts = {posts} />
-//     </div>
-//   );
-// }
-
-// const PostContainer = ({posts}) => {
-//   return (
-//     <div className="post-container">
-//       {posts.map((post , index) => (
-//         <Post key={index} post = {posts} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// const FeedSection = ({posts}) => {
-//   return (
-//     <div className="feed-section">
-//       {posts.map((post , index) => (
-//         <Post key={index} post = {post} />
-//       ))}
-//     </div>
-//   );
-// };
-
-// const Post = ({post}) => {
-//   return (
-//     <div className="post">
-//       <img src={post.avatar} alt="user-avatar" />
-//       <div className="post-content">
-//         <div className="post-header">
-//           <span className="author">{post.author}</span>
-//           <span className="time">{post.time}</span>
-//         </div>
-//         <p className="content">{post.content}</p>
-//         <img src={post.image} alt="post image" className="post-image" />
-//       </div>
-//     </div>
-
-//   );
-// };
-
-// export default App;
-
-
-  return (
-    <div id="app">
-      <h1>Enter Data</h1>
-      <PostContainer posts={posts} />
-      <FeedSection posts={posts} />
-    </div>
-  );
-}
-
-const PostContainer = ({ posts }) => {
-  return (
-    <div className="post-container">
-      {posts.map((post, index) => (
-        <Post key={index} post={post} />
-      ))}
-    </div>
-  );
-};
-
-const FeedSection = ({ posts }) => {
-  return (
-    <div className="feed-section">
-      {posts.map((post, index) => (
-        <Post key={index} post={post} />
-      ))}
-    </div>
-  );
-};
-
-const Post = ({ post }) => {
-  return (
-    <div className="post">
-      <img src={post.avatar} alt="User Avatar" />
-      <div className="post-content">
-        <div className="post-header">
-          <span className="author">{post.author}</span>
-          <span className="time">{post.time}</span>
-        </div>
-        <p className="content">{post.content}</p>
-        <img src={post.image} alt="Post Image" className="post-image" />
+    return (
+      <div id="app">
+        <PostContainer />
+        <FeedSector posts={posts}/>
       </div>
-    </div>
-  );
-};
+    )
+  }
 
-export default App;
+  const PostContainer = () => {
+    return (
+<>
+<div class="post-container">
+      <div class="post-header">
+        <img class="post-avatar" src="avatar.jpg" alt="Your Avatar" />
+        <div class="post-author">You</div>
+      </div>
+      <div class="post-content">
+        <textarea
+          class="post-input"
+          placeholder="What's on your mind?"
+        ></textarea>
+      </div>
+      <div class="post-actions">
+        <button class="post-button">Post</button>
+      </div>
+    </div></>
+    )
+  }
+
+  const FeedSector = ({posts}) => {
+    return (
+      <>
+      <Post posts={posts} />
+      </>
+    )
+  }
+
+  const Post = ({posts}) => {
+    return (
+      <>
+      {posts.map((todo , index) => (
+        <div className="post" key = {index}>
+            <div className="post-header">
+              <img
+              className="post-avatar"
+              src={todo.avatar}
+              alt={todo.author}
+              />
+            <div>
+            <div className="post-author">{todo.author}</div>
+            <div className="post-time">{todo.time}</div>
+        </div>
+        </div>
+        <div className="post-content">{todo.content}</div>  
+        <img
+          class="post-image"
+          src={todo.image}
+          alt={`Post ${index + 1}`}
+        />
+        <button>DELETE</button> 
+        </div>     
+      ))}
+      </>
+      
+    );
+  };
+
+  export default App;
